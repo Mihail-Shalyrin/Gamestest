d13e94270ef7cf1a5a2ad6ce92228dcb3e856f0a
MIhail_Shalyrin <113457077+Mihail-Shalyrin@users.noreply.github.com>
2025-04-09 17:31:54 +0300

    Add files via upload

---
+++ lcm.py
@@ -0,0 +1,16 @@
+import random as r
+from engine import run_game
+import math
+def game_logic():
+    a1 = (r.randint(1,100),r.randint(1,100),r.randint(1,100)) 
+    question = " ".join(map(str, a1))
+    answer = math.lcm(*a1)
+
+    return question,answer
+
+def play_lcm():
+    description = "Find the smallest common multiple of given numbers."
+    run_game(game_logic, description)
+    
+
+
