a4bff8a241976993d37bd58b63a2c87131b55948
MIhail_Shalyrin <113457077+Mihail-Shalyrin@users.noreply.github.com>
2025-04-09 17:42:05 +0300

    Create progression.py

---
+++ games/progression.py
@@ -0,0 +1,24 @@
+import random as r
+from engine import run_game
+
+
+
+def generate_progression():
+    a=[]
+    a.append(r.randint(1,5))
+    g = r.randint(2,9)
+    len1 = r.randint(5, 10)
+    for i in range (1,len1):
+       a.append(a[i-1]*g)
+    index = r.randint(3,len1-1)
+    anwser = a[index] 
+    a[index] = ".."
+    return " ".join(map(str, a)), anwser
+
+    
+
+
+def play_progression():
+    """Запускает игру 'Геометрическая прогрессия'."""
+    description = "What number is missing in the progression?"
+    run_game(generate_progression, description)
